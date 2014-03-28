959d4bc6184a5477176bdca8b4fe8d8b6e06c4f8
clormor <clormor@palantir.com>
2014-03-27 23:29:36 -0400

    Initial Commit

---
+++ obj-c-samples/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  obj-c-samples
+//
+//  Created by Chris Lormor on 3/27/14.
+//  Copyright (c) 2014 Chris Lormor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
