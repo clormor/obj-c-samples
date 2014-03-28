f8f93ae360e4214714c8896d4939553535222964
clormor <clormor@palantir.com>
2014-03-27 23:52:28 -0400

    adding a comment to the float calculation

--- obj-c-samples/main.m
+++ obj-c-samples/main.m
@@ -24,6 +24,8 @@ int main(int argc, const char * argv[])
         NSLog(@"The product of 8 and 9 is %i", result);
         
         NSLog(@"The quotient of 8 and 5 is %i", (8 / 5));
+        
+        // divide a float, specify a format of 1 decimal place
         NSLog(@"The quotient of 8.0 and 5.0 is %.1f", (8.0 / 5.0));
         return 0;
         
